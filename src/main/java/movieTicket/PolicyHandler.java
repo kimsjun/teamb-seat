8c8e666195d8f7acd57b6784f6a89c628aa48e4f
raamiik <warm.wiind@gmail.com>
2020-09-01 21:10:50 +0900

    seat 1st commit

--- src/main/java/movieTicket/PolicyHandler.java
+++ src/main/java/movieTicket/PolicyHandler.java
@@ -1,12 +1,16 @@
 package movieTicket;
 
 import movieTicket.config.kafka.KafkaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
+    @Autowired
+    SeatRepository seatRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
     public void onStringEventListener(@Payload String eventString){
 
@@ -16,6 +20,13 @@ public class PolicyHandler{
     public void wheneverPaymentSucceed_BookSeat(@Payload PaymentSucceeded paymentSucceeded){
 
         if(paymentSucceeded.isMe()){
+            Seat seat = new Seat();
+            seat.setBookingId(paymentSucceeded.getBookingId());
+            seat.setSeatId((long) 111);
+            seat.setSeatStatus("bookedSeat!!");
+
+            seatRepository.save(seat);
+
             System.out.println("##### listener BookSeat : " + paymentSucceeded.toJson());
         }
     }
@@ -23,9 +34,13 @@ public class PolicyHandler{
     public void wheneverUnbooked_UnbookeSeat(@Payload Unbooked unbooked){
 
         if(unbooked.isMe()){
+            Seat seat = new Seat();
+            unbooked.setBookingId(unbooked.getBookingId());
+            seat.setSeatId((long) 111);
+            seat.setSeatStatus("unbookedSeat!!");
+
+            seatRepository.save(seat);
             System.out.println("##### listener UnbookeSeat : " + unbooked.toJson());
         }
     }
-
-
 }
